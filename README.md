7a9d74d01be32a12dd322f7574b03abeb28b32d5
Alberto Pianon <alberto@pianon.eu>
2020-11-25 16:37:50 +0100

    Updated scripts for Fossology 3.8+ and added server configuration tuning

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Fossology installation script
-Simple script to install Fossology from sources and phppgadmin in Debian 8 (Jessie) or 9 (Stretch)
+Simple script to install Fossology from sources and phppgadmin in Debian 9 (Stretch) or 10 (Buster), and adjust server settings according to Fossology's [official documentation](https://github.com/fossology/fossology/wiki/Configuration-and-Tuning).
 
-After installation, you can access Fossology at https://fossology_server_address/repo, and phppgadmin at https://fossology_server_address/phppgadmin 
+After installation, you can access Fossology at https://{fossology_server_address}/repo, and phppgadmin at https://{fossology_server_address}/phppgadmin
 
 Default user/password both for Fossology and for the Postgres DB are fossy/fossy.
 
 You may want to change default password both in Fossology and in Postgres DB (because DB can be accessed via phppgadmin):
-in such case, you have to put the new DB password in `/usr/local/etc/fossology/Db.conf`, otherwise Fossology will not work.
+In case you change postgres DB password, you have to put the new DB password in `/usr/local/etc/fossology/Db.conf`, otherwise Fossology will not work.
--- install_fossology_on_debian_9_or_10.sh
+++ install_fossology_on_debian_9_or_10.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
-
-# Simple script to install Fossology from sources in Debian 9
-# Copyright (C) 2018 Alberto Pianon <pianon@array.eu>
+# SPDX-License-Identifier: GPL-3.0-only
+# SPDX-FileCopyrightText: 2020 Alberto Pianon <pianon@array.eu>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Simple script to install Fossology from sources in Debian 9-10
 
 
-FOSSOLOGY_RELEASE="3.3.0"
+fossy_release="${FOSSOLOGY_RELEASE:-3.8.1}"
 
 cp /etc/os-release .
 chmod +x os-release
 . "./os-release"
 rm os-release
-if [[ "$NAME $VERSION" == "Debian GNU/Linux 8 (jessie)" ]]; then
-  DEBIAN_VERSION="jessie"
-elif [[ "$NAME $VERSION" == "Debian GNU/Linux 9 (stretch)" ]]; then
-  DEBIAN_VERSION="stretch"
-else
-  echo "This script must be run only in Debian 8 or 9"
+if [[ "$NAME $VERSION" != "Debian GNU/Linux 9 (stretch)" ]] && [[ "$NAME $VERSION" != "Debian GNU/Linux 10 (buster)" ]]; then
+  echo "This script must be run only in Debian 9 or 10"
   exit 1
 fi
 
@@ -37,13 +21,16 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# workaround for this Debian 10 issue https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=918754
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 echo ""
 echo ""
 echo "***************************************************"
 echo "*            INSTALLING SCRIPT DEPS...            *"
 echo "***************************************************"
 apt install -y sudo build-essential git pkg-config libpq-dev libglib2.0-dev \
-  mc mawk sed software-properties-common
+  mc mawk sed software-properties-common lsb-release
 apt-add-repository non-free
 apt update
 apt install -y unrar
@@ -57,7 +44,7 @@ cd /
 git clone https://github.com/fossology/fossology.git
 set -e
 cd fossology/
-git checkout tags/$FOSSOLOGY_RELEASE
+git checkout tags/$fossy_release
 
 echo ""
 echo ""
@@ -77,54 +64,16 @@ utils/fo-installdeps -y -e
 echo ""
 echo ""
 echo "***************************************************"
-echo "*           INSTALLING PHP COMPOSER...            *"
-echo "***************************************************"
-curl -sS https://getcomposer.org/installer | php && \
-    mv composer.phar /usr/local/bin/composer
-echo "composer.phar moved to /usr/local/bin/composer"
-
-echo ""
-echo ""
-echo "***************************************************"
-echo "*           INSTALLING SPDX TOOLS...              *"
-echo "***************************************************"
-install/scripts/install-spdx-tools.sh
-
-echo ""
-echo ""
-echo "***************************************************"
-echo "*           INSTALLING NINKA...                   *"
-echo "***************************************************"
-install/scripts/install-ninka.sh
-
-if [[ "$DEBIAN_VERSION" == "stretch" ]]; then
-   echo ""
-   echo ""
-   echo "***************************************************"
-   echo "*    INSTALLING PHP 5.6 (SEEMS TO WORK BETTER)    *"
-   echo "***************************************************"
-
-   # https://stackoverflow.com/questions/46378017/install-php5-6-in-debian-9
-   # (But we cannot simply do apt install php5.6, you get errors!)
-   apt-get install -y apt-transport-https lsb-release ca-certificates
-   wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-   echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
-   apt update
-   apt install -y php5.6-cli php5.6-common php5.6-curl php5.6-gettext php5.6-json \
-      php5.6-mbstring php5.6-opcache php5.6-pgsql php5.6-readline php5.6-xml php5.6-zip php-pear \
-      libapache2-mod-php5.6
-   update-alternatives --set php /usr/bin/php5.6
-   a2dismod php7.0
-   a2enmod php5.6
-   service apache2 restart
-fi
-
-echo ""
-echo ""
-echo "***************************************************"
 echo "*            COMPILING FOSSOLOGY...               *"
 echo "***************************************************"
 make
+
+echo ""
+echo ""
+echo "***************************************************"
+echo "*           INSTALLING PHP COMPOSER...            *"
+echo "***************************************************"
+utils/install_composer.sh
 
 echo ""
 echo ""
@@ -141,30 +90,24 @@ echo "*              POST INSTALL STUFF...              *"
 echo "***************************************************"
 /usr/local/lib/fossology/fo-postinstall
 
-cp install/src-install-apache-example.conf \
-  /etc/apache2/conf-available/fossology.conf
-
-a2enconf fossology
-
-if [[ "$DEBIAN_VERSION" == "stretch" ]]; then
-   # patching php-conf-fix.sh script...
-   sed -i.bak 's/php5\/apache2\/php.ini/php\/5.6\/apache2\/php.ini/' install/scripts/php-conf-fix.sh
-fi
-
-install/scripts/php-conf-fix.sh --overwrite
-
-if [[ "$DEBIAN_VERSION" == "stretch" ]]; then
-   # restoring original script...
-   mv install/scripts/php-conf-fix.sh.bak install/scripts/php-conf-fix.sh
-fi
+# add all human users to fossy group, so that they can run fossology CLI commands
+USERS=`cut -d: -f1,3 /etc/passwd | egrep ':[0-9]{4}$' | cut -d: -f1`
+for i in $USERS; do usermod -a -G fossy $i; done
 
 a2enmod ssl
 a2ensite default-ssl
 
-cd /etc/apache2/conf-available/
+cd /etc/apache2/sites-available/
 mv fossology.conf fossology.conf.bak
 awk -vRS="AllowOverride None" -vORS="AllowOverride None\n\tSSLRequireSSL" '1' \
  fossology.conf.bak | head -n -2 > fossology.conf
+service apache2 restart
+
+echo ""
+echo ""
+echo "***************************************************"
+echo "*              INSTALLING PHPPGADMIN...           *"
+echo "***************************************************"
 
 apt install -y phppgadmin
 cd /etc/apache2/conf-available/
@@ -173,28 +116,60 @@ sed -e 's/Require local/# Require local/' phppgadmin.conf.bak | \
 awk -vRS="<Directory /usr/share/phppgadmin>"  \
     -vORS="<Directory /usr/share/phppgadmin>\nSSLRequireSSL" '1' | \
 head -n -2 > phppgadmin.conf
-
 service apache2 restart
-service fossology restart
 
-cat <<EOF > /etc/rc.local
-#!/bin/sh
-sleep 10
-systemctl restart fossology.service
-EOF
+echo ""
+echo ""
+echo "***************************************************"
+echo "*              TUNING SERVER CONFIG...            *"
+echo "***************************************************"
 
-chmod 755 /etc/rc.local
+# https://github.com/fossology/fossology/wiki/Configuration-and-Tuning#adjusting-the-kernel
+page_size=`getconf PAGE_SIZE`
+phys_pages=`getconf _PHYS_PAGES`
+shmall=`expr $phys_pages / 2`
+shmmax=`expr $shmall \* $page_size`
+echo kernel.shmmax=$shmmax >> /etc/sysctl.conf
+echo kernel.shmall=$shmall >> /etc/sysctl.conf
 
-# https://serverfault.com/questions/203863/phppgadmin-exporting-empty-sql-dump
-sed -i -e 's/$cmd = $exe . " -i";/$cmd = $exe;/g' /usr/share/phppgadmin/dbexport.php
 
-# add all human users to fossy group, so that they can run fossology scripts
-USERS=`cut -d: -f1,3 /etc/passwd | egrep ':[0-9]{4}$' | cut -d: -f1`
-for i in $USERS; do usermod -a -G fossy $i; done
+#https://github.com/fossology/fossology/wiki/Configuration-and-Tuning#preparing-postgresql
+mem=$(free --giga | grep Mem | awk '{print $2}')
+su - postgres -c psql <<EOT
+ALTER SYSTEM set shared_buffers = '$(( mem / 4 ))GB';
+ALTER SYSTEM set effective_cache_size = '$(( mem / 2 ))GB';
+ALTER SYSTEM set maintenance_work_mem = '$(( mem * 50 ))MB';
+ALTER SYSTEM set work_mem = '128MB';
+ALTER SYSTEM set fsync = 'on';
+ALTER SYSTEM set full_page_writes = 'off';
+ALTER SYSTEM set log_line_prefix = '%t %h %c';
+ALTER SYSTEM set standard_conforming_strings = 'on';
+ALTER SYSTEM set autovacuum = 'on';
+EOT
 
+#https://github.com/fossology/fossology/wiki/Configuration-and-Tuning#configuring-php
+/fossology/install/scripts/php-conf-fix.sh --overwrite
+
+#https://github.com/fossology/fossology/wiki/Email-notification-configuration#setting-up-the-email-client
+wget http://ftp.us.debian.org/debian/pool/main/s/s-nail/heirloom-mailx_14.8.16-1_all.deb
+apt install ./heirloom-mailx_14.8.16-1_all.deb
+ln -s /usr/bin/heirloom-mailx /usr/bin/mail
+
+echo ""
+echo ""
+echo "***************************************************"
+echo "*         ENABLING FOSSOLOGY SCHEDULER...         *"
+echo "***************************************************"
+
+systemctl enable fossology.service
+systemctl start fossology.service
 
 echo ""
 echo ""
 echo "***************************************************"
 echo "*                      DONE!                      *"
 echo "***************************************************"
+echo
+read -p "hit enter to reboot system"
+
+reboot
